8a3cbec1c8a79acf2705452fbaa1215962ddd660
evn <evenhold@gmail.com>
2018-01-21 22:17:28 -0500

    first commit

--- server/config/config.js
+++ server/config/config.js
@@ -1,18 +1,17 @@
 'use-strict';
 const path = require('path');
 const env = process.env.NODE_ENV || 'development';
-
 const config = {
-  development: {
-			//db: 'mongodb://localhost/yo-express-development',
-			db: 'mongodb://data1:123456@ds147167.mlab.com:47167/dbcrud'
-  },
-  test: {
-    db: 'mongodb://localhost/yo-express-test'
-  },
-  production: {
-    db: 'mongodb://localhost/yo-express-production'
-  }
+		development: {
+				//db: 'mongodb://localhost/yo-express-development',
+				db: 'mongodb://data1:123456@ds147167.mlab.com:47167/dbcrud'
+		},
+		test: {
+				db: 'mongodb://localhost/yo-express-test'
+		},
+		production: {
+				db: 'mongodb://localhost/yo-express-production'
+		}
 };
 
 module.exports = config[env];
